55942112bec8cbaa4f6dbb62432a5f2170bb4c88
DymuX17 <patrykd1707@gmail.com>
2025-11-14 22:32:18 +0100

    Updating OrderGenerator - adding logger to show up messages in container

--- producer/src/main/java/com/example/producer/service/OrderGenerator.java
+++ producer/src/main/java/com/example/producer/service/OrderGenerator.java
@@ -2,7 +2,8 @@ package com.example.producer.service;
 
 import com.example.producer.model.Order;
 import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Value;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
@@ -11,8 +12,8 @@ import java.util.UUID;
 
 @Component
 public class OrderGenerator {
-    @Value("app.kafka.topic.orders:orders}")
-    private String topic;
+    private static final String TOPIC = "orders";
+    private static final Logger logger = LoggerFactory.getLogger(OrderGenerator.class);
 
     private final KafkaTemplate<String, Order> kafkaTemplate;
     private final Random random = new Random();
@@ -26,7 +27,8 @@ public class OrderGenerator {
         Thread t = new Thread(() -> {
             while (!Thread.currentThread().isInterrupted()) {
                 Order order = generateRandomOrder();
-                kafkaTemplate.send(topic, order.getOrderId(), order);
+                logger.info("Sending order to topic: {}: {}", TOPIC, order);
+                kafkaTemplate.send(TOPIC, order.getOrderId(), order);
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
@@ -40,7 +42,7 @@ public class OrderGenerator {
     }
 
     private Order generateRandomOrder() {
-        return new Order(UUID.randomUUID().toString(),
+        return new Order(UUID.randomUUID().toString().substring(0, 5),
                 "user-" + random.nextInt(10),
                 Math.round((10 + random.nextDouble() * 990) * 100.0) / 100.0);
     }
